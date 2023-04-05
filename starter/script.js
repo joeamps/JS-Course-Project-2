07106201b1de9d295e8949512e9e43ea75b65ed7
joeamps <amper.j@gmercyu.edu>
2023-04-05 11:01:51 -0400

    final code

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,16 @@
 'use strict';
+
+// define variables
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+
+
+// setting default values to 0
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden'); // hides the dice upon refresh
+
+// game functionality
+
+
